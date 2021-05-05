d5464d8fcd8271c2805d0990a3171ee2d6090725
Vika-Derkach <queenviktoriyphp@gmail.com>
2021-05-05 23:50:19 +0300

    catalog-page

--- src/components/goods-item/goods-item.js
+++ src/components/goods-item/goods-item.js
@@ -2,8 +2,19 @@ import React from "react";
 import "./goods-item.css";
 const GoodsItem = () => {
   return (
-    <div>
-      <h1>dcd</h1>
+    <div className="goods-item-container">
+      <div className="good-item">
+        <img
+          src="https://handcmediastorage.blob.core.windows.net/productimages/JK/JKPRAS01-F30-121326-800px-1040px.jpg"
+          alt=""
+        />
+      </div>
+      <div className="good-item">
+        <img
+          src="https://handcmediastorage.blob.core.windows.net/productimages/JK/JKPRAS01-F30-121326-800px-1040px.jpg"
+          alt=""
+        />
+      </div>
     </div>
   );
 };
